52b81fafb98e30f49aefd17940ed96231bf0d244
Rodrigo Luque <67666108+SirFreddie@users.noreply.github.com>
2021-09-28 16:08:20 -0300

    Commit Inicial

---
+++ src/Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ src/Test/Library.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Proyecto_Final
+{
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
