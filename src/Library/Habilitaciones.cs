ec82851c0d5509cbd5dbafb039e404cedefd76f8
Néstor Martínez <nestor.marvi@gmail.com>
2021-10-24 15:59:29 -0300

    Agregada clase habilitaciones

--- src/Library/Habilitaciones.cs
+++ src/Library/Habilitaciones.cs
@@ -1,14 +1,52 @@
 using System;
+using System.Collections;
 
 namespace Proyecto_Final
 {
     public class Habilitaciones
     {
-        public string Nombre { get; }
+        public ArrayList ListaHabilitaciones { get; private set;}
 
-        public Habilitaciones(string nombre)
+        public Habilitaciones(ArrayList listaHabilitaciones)
         {
-            this.Nombre = nombre;
+            this.ListaHabilitaciones = listaHabilitaciones;
+        }
+
+        public void AgregarHabilitacion(string habilitacion)
+        {
+            this.ListaHabilitaciones.Add(habilitacion);
+        }
+        public void RemoverHabilitacion(string habilitacion)
+        {
+            this.ListaHabilitaciones.Remove(habilitacion);
+        }
+
+        public bool CheckHabilitaciones(ArrayList habilitacionesACheckear)
+        {
+            foreach(string habilitacionACheckear in habilitacionesACheckear)
+            {
+                foreach(string habilitacion in this.ListaHabilitaciones)
+                {
+                    if(habilitacion.ToLower() == habilitacionACheckear.ToLower())
+                    {
+                        Console.WriteLine($"El emprendedor contiene la habilitación {habilitacionACheckear}");
+                        habilitacionesACheckear.Remove(habilitacionACheckear);
+                    }
+                }  
+            }
+            if (habilitacionesACheckear.Count == 0)
+            {
+                Console.WriteLine($"El emprendedor contiene todas las habilitaciones requeridas.");
+                return true;
+            }
+            else
+            {
+                foreach(string habilitacionACheckear in habilitacionesACheckear)
+                {
+                    Console.WriteLine($"El emprendedor necesita tener {habilitacionesACheckear} para aceptar el contrato.");
+                }
+            }
+            return false; 
         }
     }
 }
