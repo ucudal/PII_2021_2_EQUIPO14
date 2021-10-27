4d7217000ddd55109605142a8d9d189026b9eecb
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2021-10-27 18:18:37 -0300

    Clases agregadas

---
+++ src/Library/Rubro.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+
+namespace Proyecto_Final
+{
+    public class Rubro
+    {
+        public ArrayList Rubros { get; }
+        public Rubro(ArrayList rubros)
+        {
+            this.Rubros = rubros;
+        }
+    }
+}
---
+++ src/Library/UserEmpresa.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Proyecto_Final
+{
+    public class UserEmpresa
+    {
+        public string Nombre { get; }
+        public Empresa Empresa { get; private set; }
+
+        public UserEmpresa(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        /* Como empresa, quiero aceptar una invitación a unirme en la plataforma y registrar mi nombre, 
+        ubicación y rubro, para que de esa forma pueda comenzar a publicar ofertas. */
+        public void AceptarInvitacion()
+        {
+            Console.WriteLine("Aceptar invitacion? Y/N");
+            string input = Console.ReadLine().ToUpper();   
+            if (input == "Y")
+            {
+                Console.WriteLine("Invitacion aceptada...");
+                Console.WriteLine("Ingrese el nombre de su empresa: ");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese la direccion de su empresa: ");
+                string ubicacion = Console.ReadLine();
+                Console.WriteLine("Ingrese el rubro de su empresa (podra agregar mas rubros luego si lo necesita): ");
+                string rubro = Console.ReadLine();
+                ArrayList rubros = new ArrayList();
+                rubros.Add(rubro);
+
+                // Deberia UserEmpresa ser la encargada de crear la empresa y el rubro? (Creator)
+                Rubro newRubro = new Rubro(rubros);
+                this.Empresa = new Empresa(nombre, ubicacion, newRubro);
+                Console.WriteLine($"Empresa: {this.Empresa.Nombre} creada!.");
+            }
+            else
+            {
+                Console.WriteLine("Invitacion rechazada...");
+            }
+        }
+
+        public void AgregarRubro()
+        {
+            this.Empresa.AgregarRubro(); // (Delegacion)
+        }
+
+        public void EliminarRubro()
+        {
+            this.Empresa.EliminarRubro(); // (Delegacion)
+        }
+
+        public void AgregarEspecializacion()
+        {
+            this.Empresa.AgregarEspecializacion(); // (Delegacion)
+        }
+
+        public void EliminarEspecializacion()
+        {
+            this.Empresa.EliminarEspecializacion(); // (Delegacion)
+        }
+
+        /* Como empresa, quiero indicar un conjunto de palabras claves asociadas a la publicación de los materiales, 
+        para que de esa forma sea más fácil de encontrarlos en las búsquedas que hacen los emprendedores. */
+        public void CrearMsjClave(Oferta oferta)
+        {
+            this.Empresa.AgregarMsjClave(oferta); // (Delegacion)
+        }
+
+        /* Como empresa, quiero publicar una oferta de materiales reciclables o residuos, 
+        para que de esa forma los emprendedores que lo necesiten puedan reutilizarlos. */
+        public void CrearOferta()
+        {
+            Console.WriteLine("Ingrese el nombre de su publicacion: ");
+            string nombre = Console.ReadLine();
+            Oferta newOferta = new Oferta(nombre);
+
+            this.Empresa.Ofertas.Add(newOferta);
+            Console.WriteLine($"Oferta {newOferta} publicada!.");
+        }
+    }
+}
