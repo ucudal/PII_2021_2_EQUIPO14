7847871082f587e7bb43592990f7f5bde6654b1f
Néstor Martínez <nestor.marvi@gmail.com>
2021-10-31 20:45:32 -0300

    Documentacion en Singleton

--- src/Library/Singleton.cs
+++ src/Library/Singleton.cs
@@ -1,10 +1,17 @@
 namespace Proyecto_Final
 {
+    /// <summary>
+    /// Esta clase tiene como función funcionar como base para crear una sola instancia de clases almacenadoras de datos en el programa.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Singleton<T> where T : new()
     {
     
         private static T instance;
-
+        /// <summary>
+        /// Otorga una instancia de la clase que sea Singleton.
+        /// </summary>
+        /// <value><c>true</c>Crea una instancia de la clase Singleton y la retorna,<c>false</c> solo retorna la instancia de clase ya existente.</value>
         public static T Instance
         {
             get
