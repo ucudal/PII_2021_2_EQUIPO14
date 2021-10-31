693eb71dc04d27cd6d16845516255581a515bce2
Néstor Martínez <nestor.marvi@gmail.com>
2021-10-31 20:00:53 -0300

    Agregados los checks

--- src/Library/Datos.cs
+++ src/Library/Datos.cs
@@ -4,6 +4,7 @@ namespace Proyecto_Final
 {
     public class Datos
     {
+        
         private ArrayList listaHabilitaciones = new ArrayList(); 
         public ArrayList ListaHabilitaciones()
         {
@@ -24,49 +25,72 @@ namespace Proyecto_Final
 
         public void AgregarHabilitacion(Habilitaciones habilitacion)
         {
-            ListaHabilitaciones.Add(habilitacion);
+            listaHabilitaciones.Add(habilitacion);
         }
 
         public void EliminarHabilitacion(Habilitaciones habilitacion)
         {
-            ListaHabilitaciones.Remove(habilitacion);
+            listaHabilitaciones.Remove(habilitacion);
         }
 
         public void AgregarRubro(Rubro rubro)
         {
-            ListaRubros.Add(rubro);
+            listaRubros.Add(rubro);
         }
 
         public void EliminarRubro(Rubro rubro)
         {
-            ListaRubros.Remove(rubro);
+            listaRubros.Remove(rubro);
         }
 
         public void AgregarTipo(TipoProducto tipo)
         {
-            ListaTipos.Add(tipo);
+            listaTipos.Add(tipo);
         }
 
         public void EliminarTipo(TipoProducto tipo)
         {
-            ListaTipos.Remove(tipo);
+            listaTipos.Remove(tipo);
         
         }
 
         public bool CheckHabilitaciones(string habilitacion)
         {
-            //Singleton<Datos>.Instance.ListaHabilitaciones(); 
-
+            Habilitaciones habilitacionACheckear = new Habilitaciones(habilitacion);
+            foreach(Habilitaciones habilitacionAlmacenada in Singleton<Datos>.Instance.ListaHabilitaciones())
+            {
+                if(habilitacionACheckear == habilitacionAlmacenada)
+                {
+                    return true;
+                }
+            }
+            return false; 
         }
 
-        public bool CheckTipos(string habilitacion)
+        public bool CheckTipos(string tipoProducto)
         {
-            
+            TipoProducto tipoACheckear = new TipoProducto(tipoProducto);
+            foreach(TipoProducto tiposAlmacenados in Singleton<Datos>.Instance.ListaTipos())
+            {
+                if(tipoACheckear == tiposAlmacenados)
+                {
+                    return true;
+                }
+            }
+            return false; 
         }
 
-        public bool CheckRubros(string habilitacion)
+        public bool CheckRubros(string rubro)
         {
-            
+            Rubro rubroACheckear = new Rubro(rubro);
+            foreach(Rubro rubroAlmacenado in Singleton<Datos>.Instance.ListaRubros())
+            {
+                if(rubroACheckear == rubroAlmacenado)
+                {
+                    return true;
+                }
+            }
+            return false; 
         }
     }
 }
