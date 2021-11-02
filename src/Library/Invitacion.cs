305604601e94058d772b0f8a98e58d29e4a21fe6
Rodrigo Luque <67666108+SirFreddie@users.noreply.github.com>
2021-11-02 19:03:46 -0300

    Comentarios XML en invitacion

--- src/Library/Invitacion.cs
+++ src/Library/Invitacion.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 namespace Proyecto_Final
 {
+    /// <summary>
+    /// Esta clase representa una invitacion.
+    /// </summary>
     public class Invitacion
     {
+        /// <summary>
+        /// Inicializa la clase Invitacion.
+        /// </summary>
         public Invitacion()
         {
 
