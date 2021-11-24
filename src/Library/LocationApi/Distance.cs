6e2a1c88b7ca7543faa5305662a1f57f83a8c5d5
Juan <juan.lupi@correo.ucu.edu.uy>
2021-11-24 18:57:10 -0300

    Agregada nueva Location Api

--- src/Library/LocationApi/Distance.cs
+++ src/Library/LocationApi/Distance.cs
@@ -8,19 +8,20 @@ namespace Ucu.Poo.Locations.Client
     public class Distance
     {
         /// <summary>
-        /// Indica si se encontraron o no las coordenas o las direcciones de origen y destino.
+        /// Obtiene o establece un valor que indica si se encontraron o no las coordenas o las direcciones de origen y
+        /// destino.
         /// </summary>
         /// <value>true si se encontró la dirección; false en caso contrario.</value>
         public bool Found { get; set; }
 
         /// <summary>
-        /// La distancia entre las coordenadas o las direcciones de origen y destino.
+        /// Obtiene o establece la distancia entre las coordenadas o las direcciones de origen y destino.
         /// </summary>
         /// <value>La distancia en metros.</value>
         public double TravelDistance { get; set; }
 
         /// <summary>
-        /// El tiempo que se demora en llegar de las coordenadas o de las direcciones de origen a las de destino.
+        /// Obtiene o establece el tiempo que se demora en llegar de las coordenadas o de las direcciones de origen a las de destino.
         /// </summary>
         /// <value>El tiempo que se demora en minutos.</value>
         public double TravelDuration { get; set; }
