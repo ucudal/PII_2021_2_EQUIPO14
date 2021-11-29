25e2a7186e6af3c142dab2b9f6f697fde5377822
Néstor Martínez <nestor.marvi@gmail.com>
2021-11-29 20:19:05 -0300

    Parche EndOfferHandler

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -21,7 +21,7 @@ namespace Proyecto_Final
         // *Importante*:
         // Para probar este ejemplo, crea un bot nuevo y eeemplaza este token por el de tu bot.
 
-        private static string Token = "2004584466:AAEgtrmxu4kgJwBAomfmRPuovsGTI9pAwAg";
+        private static string Token = "2135269832:AAFLneVWbn_FduSGcpKCCIfiIJyDzoH3oTk";
 
         private static IHandler firstHandler;
 
